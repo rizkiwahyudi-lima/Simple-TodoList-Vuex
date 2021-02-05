e194cdd71d5b0d54979fa0b757c2844e3646b4e9
rizkiwahyudi-lima <rizkiwahyudi-lima>
2021-02-05 17:06:19 +0700

    Fetching API Todo List

---
+++ src/components/Todos.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="todos">
+      <!-- For in Vue, use v-for and v-bind:key -->
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+        {{ todo.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Import mapGatters vuex
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Todos",
+  methods: {
+    //   binding namespace module
+    ...mapActions(["fetchTodos"]),
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  },
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #16c79a;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+</style>
---
+++ src/store/modules/todos.js
@@ -0,0 +1,31 @@
+// Place for tag state, getter, actions and mutations
+import axios from 'axios';
+
+const state = {
+    todos: [
+
+    ]
+};
+
+const getters = {
+    allTodos: state => state.todos
+};
+
+const actions = {
+    async fetchTodos({ commit }) {
+        // Memanggil API Todos
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        commit('setTodos', response.data);
+    }
+};
+
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
